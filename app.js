b4881ef0bad7a9eb16d4961b93d79175408fba3a
azizahmedkhan <azizahmed.khan@gmail.com>
2025-04-22 20:45:00 +1200

    Adding home page

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 console.log("Application is starting");
 
 const express = require('express')
+const ejs = require('ejs')
 const app = express()
 const port = 3000
+app.set('view engine','ejs'); 
+app.use(express.static('public')) // to serve static files
+
+
 
 app.get("/", (req, res, next) => {
     console.log("I received a get request on the path // ")
-    res.send("<h1>Acknowledged</h1>")
+    let html = ejs.render("homepage.ejs");
+    res.render(html)
 })
+
+app.get("/welcome", (req, res, next) => {
+  console.log("I received a get request on the path // ")
+  res.send("<h1 style='color:blue;'>Welcome</h1>")
+})
+
+
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
